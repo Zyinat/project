7670ad105b830498ed2770f8d1268402af3f7488
unknown <zyinat.kurbanbaeva.kk@gmail.com>
2022-03-23 16:12:16 +0300

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[]array={1,2,3,4,5,6,7,8,9,10,};
+        System.out.println(array[1 ]);
+        System.out.println(array[3]);
+        System.out.println(array[5]);
+        System.out.println(array[7]);
+        System.out.println(array[9]);
+
+
+    }
+
+    }
+
+	// write your code here
+
+
